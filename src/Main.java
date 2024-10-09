8c05cdb2dec433c831a7569e9d4cba66b3a44071
Thang0226 <thang.nd0226@gmail.com>
2024-10-09 16:59:45 +0700

    update prompt

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter an integer number (0 < number < 1000):");
+        System.out.println("Enter an integer number (0 <= number < 1000):");
         int number = scanner.nextInt();
 
         if (number < 0 || number >= 1000) {
